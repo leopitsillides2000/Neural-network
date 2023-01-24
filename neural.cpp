6a829b213c5755987b1628c6e69de4d49c1af7d0
Leo Pitsillides <leo.pitsillides@gmail.com>
2023-01-24 15:30:01 +0000

    Creates NeuralNetwork class that initialises the necessary inputs and it also contains the methods to implement the ANN. Strungling to grasp the required sizes for all the different inputs.

--- neural.cpp
+++ neural.cpp
@@ -4,29 +4,77 @@
 
 using namespace std;
 
-float sigmoid(int z);
-float mse(vector<int> y_true, vector<float> y_check);
+class NeuralNetwork;
 
 int main(){
+    NeuralNetwork neural;
 
-    vector<int> y = {1,0,0,1,1,0,1};
-    vector<float> y_hat = {0.24,0.1,0.3,0.91,0.87,0.4,0.665};
+    //initialising 
+    neural.input = ;
+    neural.weights1 = ;
+    neural.weights2 = ;
+    neural.output
 
-    cout << mse(y, y_hat) << endl;
     return 0;
 }
 
+class NeuralNetwork{
+    public:
+        vector<int> input;
+        vector<float> weights1;
+        vector<float> weights2;
+        vector<float> output;
 
-float sigmoid(int z){
-    return 1/(1+exp(-z));
-}
 
-float mse(vector<int> y_true, vector<float> y_check){
-    float error;
-    int n = sizeof(y_true)/sizeof(y_true[0]);
+        void sigmoid(vector<float> x){
+            int n = sizeof(x)/sizeof(x[0]);
 
-    for (int i = 0; i < n; i++){
-        error += pow(y_check[i] - y_true[i], 2);
-    }
-    return error/n;
+            for (i=0;i<n;i++){
+                x[i] = 1/(1+exp(-x[i]));
+            }
+
+            };
+
+        vector<float> sigmoid_der(vector<float> x){
+            return sigmoid(x)*(1-sigmoid(x));
+        };
+
+        float mse(vector<float> y_true, vector<float> y_check){
+            float error;
+            int n = sizeof(y_true)/sizeof(y_true[0]);
+
+            for (int i = 0; i < n; i++){
+                error += pow(y_check[i] - y_true[i], 2);
+            }
+            return error/n;
+        };
+
+        float dot_product(vector<float> x_vec, vector<float> y_vec){
+            int n = sizeof(y_true)/sizeof(y_true[0]);
+            float total = 0;
+
+            for (int i = 0; i<n; i++){
+                total += x_vec[i]*y_vec[i];
+                }
+            return total;
+        };
+
+        void grad_descent(float weight, float z, float a){
+            weight = weight - a*sigmoid_der(z);
+        };
+
+        void feedforward(vector<int> input, vector<float> weights1, vector<float> weights2){
+            
+            //input should be a vector of vectors
+            //each vector in input is dot product with a vector of weights so weights1 is a vector of vectors
+            //this then gives a simple vector so layer1 will be a vector
+            //layer1 is dot product with weights2, a vector, to give a single output value
+
+            vector<float> layer1 = sigmoid(dot_product(input, weights1));
+            output = sigmoid(dot_product(layer1, weights2));
+        };
+
+        void backprop(){
+
+        };
 }
